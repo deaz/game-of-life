29326777f85b9d80d5df05c9b815e0ed5202c4e1
Ivan Vologdin <vologdin.nsk@gmail.com>
2013-07-27 14:01:28 +0700

    Add pipe instead of queue for better perfomance

--- gameoflife/graphicalgrid.py
+++ gameoflife/graphicalgrid.py
@@ -1,23 +1,19 @@
 import multiprocessing
-import queue
 from .cellgrid import CellGrid
 
 
 class GraphicalGrid(CellGrid):
-    MAX_BUFFER_SIZE = 5
-
     def __init__(self, cols_count, rows_count, cell_width, canvas):
         super().__init__(cols_count, rows_count)
         self.cell_width = cell_width
         self.canvas = canvas
         self.old_rects = {}
-        self.cells_buffer = multiprocessing.Queue(GraphicalGrid.MAX_BUFFER_SIZE)
+        self.recv_conn, self.send_conn = multiprocessing.Pipe(False)
 
     def draw(self):
-        try:
-            alive_cells = self.cells_buffer.get_nowait()
-        except queue.Empty:
+        if not self.recv_conn.poll():
             raise BufferEmpty()
+        alive_cells = self.recv_conn.recv()
         old_cells_indices = set(self.old_rects.keys())
         cells_to_remove = old_cells_indices.difference(alive_cells)
         cells_to_draw = alive_cells.difference(old_cells_indices)
@@ -34,7 +30,7 @@ class GraphicalGrid(CellGrid):
 
     def update(self):
         super().update()
-        self.cells_buffer.put(self.get_alive_cells())
+        self.send_conn.send(self.get_alive_cells())
 
 
 class BufferEmpty(Exception):
